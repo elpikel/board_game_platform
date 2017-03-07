e282485fde7564959069c26815c1de033915cb2f
Arkadiusz Plichta <el.pikel@gmail.com>
2017-03-07 14:24:58 +0100

    gryplanszowe basic tasks

--- apps/gryplanszowe/lib/gryplanszowe.ex
+++ apps/gryplanszowe/lib/gryplanszowe.ex
@@ -13,6 +13,16 @@ defmodule Gryplanszowe do
 
   """
   def get_games do
+    # http://www.gryplanszowe24.pl/639-gry-planszowe
+    # get http content
+    # parse http content
+    # make domain models
+    # save to local db
+
+    # get pages
+    # make worker for each page
+    # add supervisor
+    # think about caching
     []
   end
 end
