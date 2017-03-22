c01f75c2f532105f67608e552da40546c95dee92
Arkadiusz Plichta <el.pikel@gmail.com>
2017-03-22 19:53:46 +0100

    home page basic look

--- apps/board_game_web/web/controllers/page_controller.ex
+++ apps/board_game_web/web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule BoardGameWeb.PageController do
   use BoardGameWeb.Web, :controller
 
   def index(conn, _params) do
+    # get data from board_game_repo and display
     render conn, "index.html"
   end
 end
